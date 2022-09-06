8faa77585b50ca896bae057594ae5ce149b25acc
mahmoudhammad309 <mahmoudhammad3009@gmail.com>
2022-09-06 17:47:29 +0300

    create the functionality for logout and modify the style

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,11 @@
 const getPosts = require('./getPosts');
 const signup = require('./signup');
 const logIn = require('./login');
+const logout = require('./logout');
 
 module.exports = {
   getPosts,
   signup,
   logIn,
+  logout,
 };
