f66a2c9c74f74d94e7d1bb9e93c86187708f58a2
mahmoudhammad309 <mahmoudhammad3009@gmail.com>
2022-09-07 09:19:19 +0300

    add the create post button and style it & test some routes

--- server/router/index.js
+++ server/router/index.js
@@ -25,7 +25,6 @@ router.route('/login')
   )
   .post(logIn);
 
-router.get('/logout', logout);
 router.get('/posts', getPosts);
 
 router.get('/homePage', (req, res, next) => {
@@ -42,5 +41,7 @@ router.get('/homePage', (req, res, next) => {
     res.redirect('/');
   }
 });
+
+router.get('/logout', logout);
 
 module.exports = router;
